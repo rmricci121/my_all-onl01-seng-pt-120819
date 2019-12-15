330fe11d133f2ca102d87cc6c99964dfa5857c34
Robert M Ricci <rmricci121@gmail.com>
2019-12-15 01:20:17 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,10 @@
 require 'pry'
 
 def my_all?(collection)
-
+  block_return_values = []
+  i = 0 
+  while i < collection.length 
+   block_return_values << yield(collection[i])
+    i += 1 
+  end
 end
